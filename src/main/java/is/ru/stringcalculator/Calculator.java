da46874b64c2c57620e1ce38829b9a2bda0c310a
johannbjarka <johannbjarka@gmail.com>
2014-10-17 17:28:52 +0000

    Added test for handling different delimiters, changed the scripts to work with Windows

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,6 +6,12 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
+		else if(text.startsWith("//")){
+			char delim = text.charAt(2);
+			String delimiter = Character.toString(delim);
+			String numbers = text.substring(4);
+			return sum(splitNumbers(numbers, delimiter));
+		}
 		else if(text.contains(",") || text.contains ("\n")){
 			return sum(splitNumbers(text));
 		}
@@ -19,6 +25,10 @@ public class Calculator {
 
 	private static String[] splitNumbers(String numbers){
 	    return numbers.split(",|\n");
+	}
+	
+	private static String[] splitNumbers(String numbers, String delimiter){
+		return numbers.split(delimiter);
 	}
       
     private static int sum(String[] numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,8 +29,11 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 	@Test
-   public void testNewlineDelimiter(){
-	assertEquals(6, Calculator.add("1\n2,3"));
+    public void testNewlineDelimiter(){
+		assertEquals(6, Calculator.add("1\n2,3"));
     }
-
+	@Test
+	public void testHandleDifferentDelimiter(){
+		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
 }
