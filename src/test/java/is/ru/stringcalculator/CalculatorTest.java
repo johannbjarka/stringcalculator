bbd8f2950ef1ffbb73d9e8234df40e11f7f74659
Johann Bjarkason <johann@i-0f994fee.greenqloud.internal>
2014-10-17 16:33:34 +0000

    Added test for a newline delimiter

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -22,11 +22,15 @@ public class CalculatorTest {
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
-	}	
+	}
 
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
+    }
+	@Test
+   public void testNewlineDelimiter(){
+	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
 }
