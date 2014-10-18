2ea36ac775fec716b4d2e477a8f36b102257f30d
johannbjarka <johannbjarka@gmail.com>
2014-10-18 01:57:52 +0000

    Added test for handling multiple delimiters of any length

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -63,4 +63,9 @@ public class CalculatorTest {
 	public void testMultipleDelimiters(){
 		assertEquals(6, Calculator.add("//[*][%]\n1*2%3"));
 	}
+	@Test
+	public void testMultipleDelimitersOfAnyLength(){
+		assertEquals(10, Calculator.add("//[***][%%][;;;;;]\n1***2%%3;;;;;4"));
+		assertEquals(15, Calculator.add("//[..][,,,][#][!!!!]\n1..2,,,3#4!!!!5"));
+	}
 }
